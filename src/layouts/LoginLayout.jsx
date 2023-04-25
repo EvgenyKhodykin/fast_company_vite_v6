292a34a8c2528f5d54d7b273e27b6433f876c10a
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-04-25 17:55:17 +0500

    current code

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react'
+import { React, useEffect, useState } from 'react'
 import TextField from '../components/TextField'
 
 function LoginLayout() {
     const [data, setData] = useState({ email: '', password: '' })
+    const [errors, setErrors] = useState({})
 
     const handleChange = ({ target }) => {
         setData(prevState => ({ ...prevState, [target.name]: target.value }))
     }
 
+    useEffect(() => {
+        validate()
+    }, [data])
+
+    const validate = () => {
+        const errors = {}
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === '') {
+                errors[fieldName] = `${fieldName} обязательно для заполнения`
+            }
+        }
+        setErrors(errors)
+        return Object.keys(errors).length === 0 || false
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const isValid = validate()
+        if (!isValid) return
+        console.log(data)
+    }
+
     return (
-        <form action=''>
+        <form onSubmit={handleSubmit}>
             <TextField
                 label='Email'
                 name='email'
                 value={data.email}
                 onChange={handleChange}
+                error={errors.email}
             />
             <TextField
                 label='Пароль'
@@ -22,7 +46,9 @@ function LoginLayout() {
                 name='password'
                 value={data.password}
                 onChange={handleChange}
+                error={errors.password}
             />
+            <button type='button'>Submit</button>
         </form>
     )
 }
