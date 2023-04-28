9fe8759e76fd010f3e42b1974367ae2456b33f28
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-04-28 14:08:40 +0500

    current validation

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -49,6 +49,8 @@ function LoginLayout() {
         return Object.keys(errors).length === 0
     }
 
+    const isValid = Object.keys(errors).length === 0
+
     const handleSubmit = event => {
         event.preventDefault()
         const isValid = validate()
@@ -57,24 +59,36 @@ function LoginLayout() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label='Email'
-                name='email'
-                value={data.email}
-                onChange={handleChange}
-                error={errors.email}
-            />
-            <TextField
-                label='Пароль'
-                type='password'
-                name='password'
-                value={data.password}
-                onChange={handleChange}
-                error={errors.password}
-            />
-            <button>Submit</button>
-        </form>
+        <div className='container mt-5'>
+            <div className='row '>
+                <div className='col-md-6 offset-md-3 shadow p-4'>
+                    <h3 className='mb-4'>Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label='Email'
+                            name='email'
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label='Пароль'
+                            type='password'
+                            name='password'
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            disabled={!isValid}
+                            className='btn btn-primary w-100 mx-auto'
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
