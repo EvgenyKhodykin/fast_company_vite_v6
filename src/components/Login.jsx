e47617befbca08bcee99df2cd7a5ab2211ec8825
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-04-29 13:48:34 +0500

    Login component has been added

---
+++ src/components/Login.jsx
@@ -0,0 +1,95 @@
+import { React, useEffect, useState } from 'react'
+import TextField from './TextField'
+import validator from '../utils/validator'
+
+function Login() {
+    const [data, setData] = useState({ email: '', password: '' })
+    const [errors, setErrors] = useState({})
+
+    const handleChange = ({ target }) => {
+        setData(prevState => ({
+            ...prevState,
+            [target.name]: target.value
+        }))
+    }
+
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: 'Поле Email обязательно для заполнения'
+            },
+            isEmail: {
+                message: 'Email введён некорректно'
+            }
+        },
+        password: {
+            isRequired: {
+                message: 'Поле Пароль обязательно для заполнения'
+            },
+            isCapitalSymbol: {
+                message: 'Пароль должен содержать хотя бы одну заглавную букву'
+            },
+            isContainDigit: {
+                message: 'Пароль должен содержать хотя бы одну цифру'
+            },
+            min: {
+                message: 'Пароль должен состоять минимум из 8 символов',
+                value: 8
+            }
+        }
+    }
+
+    useEffect(() => {
+        validate()
+    }, [data])
+
+    const validate = () => {
+        const errors = validator(data, validatorConfig)
+        setErrors(errors)
+        return Object.keys(errors).length === 0
+    }
+
+    const isValid = Object.keys(errors).length === 0
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const isValid = validate()
+        if (!isValid) return
+        console.log(data)
+    }
+
+    return (
+        <div className='container mt-5'>
+            <div className='row '>
+                <div className='col-md-6 offset-md-3 shadow p-4'>
+                    <h3 className='mb-4'>Autorization</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label='Email'
+                            name='email'
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label='Password'
+                            type='password'
+                            name='password'
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            disabled={!isValid}
+                            className='btn btn-primary w-100 mx-auto'
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
