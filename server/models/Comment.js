74ba73fe28114f84f4774c8adfe8ce8cdbb58a07
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-08-07 20:56:37 +0500

    all models is done

---
+++ server/models/Comment.js
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose'
+
+const schema = new Schema(
+    {
+        content: { type: String, required: true },
+        pageId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+    },
+    {
+        timestamps: { createdAt: 'created_at' }
+    }
+)
+
+export default model('Comment', schema)
---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose'
+
+const schema = new Schema(
+    {
+        name: String,
+        email: { type: String, required: true, unique: true },
+        password: String,
+        completedMeetings: Number,
+        image: String,
+        profession: { type: Schema.Types.ObjectId, ref: 'Profession' },
+        qualities: [{ type: Schema.Types.ObjectId, ref: 'Quality' }],
+        rate: Number,
+        sex: { type: String, enum: ['male', 'female'] }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default model('User', schema)
