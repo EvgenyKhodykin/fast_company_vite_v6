b2186f153d296c2ce241c6f915353c7ccc55a7a4
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-08-08 19:36:42 +0500

    auth routes is done

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -4,6 +4,7 @@ import { check, validationResult } from 'express-validator'
 import User from '../models/User.js'
 import { generateUserData } from '../utils/helpers.js'
 import tokenService from '../services/token.service.js'
+import Token from '../models/Token.js'
 
 const authRouter = express.Router({ mergeParams: true })
 
@@ -48,7 +49,9 @@ authRouter.post('/signUp', [
 
             response.status(201).send({ ...tokens, userId: newUser._id })
         } catch (error) {
-            response.status(500).json({ message: 'Server error.Try again later...' })
+            response
+                .status(500)
+                .json({ message: 'Ошибка на сервере.Попробуйте позднее...' })
         }
     }
 ])
@@ -81,7 +84,7 @@ authRouter.post('/signInWithPassword', [
                 })
             }
 
-            const isPasswordEqual = await bcrypt.compare(password, existingUser.password)
+            const isPasswordEqual = bcrypt.compare(password, existingUser.password)
             if (!isPasswordEqual) {
                 response.status(400).send({
                     error: {
@@ -96,11 +99,37 @@ authRouter.post('/signInWithPassword', [
 
             response.status(200).send({ ...tokens, userId: existingUser._id })
         } catch (error) {
-            response.status(500).json({ message: 'Server error.Try again later...' })
+            response
+                .status(500)
+                .json({ message: 'Ошибка на сервере.Попробуйте позднее...' })
         }
     }
 ])
 
-authRouter.post('/token', async (request, response) => {})
+function isTokenInvalid(data, dbToken) {
+    return !data || !dbToken || data._id !== dbToken?.user?.toString()
+}
+
+authRouter.post('/token', async (request, response) => {
+    try {
+        const { refresh_token: refreshToken } = request.body
+        const data = tokenService.validateRefresh(refreshToken)
+        const dbToken = await tokenService.findToken(refreshToken)
+
+        if (isTokenInvalid(data, dbToken)) {
+            return response.status(401).json({ message: 'Unauthorized' })
+        }
+
+        const tokens = tokenService.generate({
+            _id: data._id
+        })
+
+        await tokenService.save(data._id, tokens.refreshToken)
+
+        response.status(200).send({ ...tokens, userId: data._id })
+    } catch (error) {
+        response.status(500).json({ message: 'Ошибка на сервере.Попробуйте позднее...' })
+    }
+})
 
 export default authRouter
